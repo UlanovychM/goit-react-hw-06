467e33831e9a774fd17e2eeed4c3d9aff0031197
Mykhailo <m.ulanovych@gmail.com>
2024-05-02 22:40:23 +0300

    'update'

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistR } from './redux/strore.js';
+import { store, persistR } from './redux/store.js';
 import App from './components/App/App.jsx';
 
 import './index.css';
